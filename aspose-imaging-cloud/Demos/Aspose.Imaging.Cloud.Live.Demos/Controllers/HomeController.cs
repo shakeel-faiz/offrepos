39800067418f4a93ac9ba3bd9ed9cdfd49c3483a
ShakeelFaiz <shakeel.faiz@aspose.com>
2021-07-12 19:14:17 +0500

    Added imaging resources file

--- aspose-imaging-cloud/Demos/Aspose.Imaging.Cloud.Live.Demos/Controllers/HomeController.cs
+++ aspose-imaging-cloud/Demos/Aspose.Imaging.Cloud.Live.Demos/Controllers/HomeController.cs
@@ -14,8 +14,8 @@ namespace Aspose.Imaging.Cloud.Live.Demos.Controllers
 
         public IActionResult Default()
         {
-            ViewBag.PageTitle = Resources["cadConversionPageTitle"];
-            ViewBag.MetaDescription = Resources["cadConversionMetaDescription"];
+            ViewBag.PageTitle = Resources["imagingConversionPageTitle"];
+            ViewBag.MetaDescription = Resources["imagingConversionMetaDescription"];
 
             var model = new LandingPageModel(this);
             return View(model);
